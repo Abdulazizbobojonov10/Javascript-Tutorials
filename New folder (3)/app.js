577872f79a8454e79071249308c50e962d927797
Abdulazizbobojonov10 <bobojonovabdulaziz3@gmail.com>
2024-02-05 10:06:42 +0300

    done

---
+++ New folder (3)/app.js
@@ -0,0 +1,17 @@
+// const son  = prompt("salom son kiriting");
+// const daraja = prompt("endi darajasini kiriting ");
+// let natija =  son ** daraja;
+
+// console.log(natija);
+
+const son_1 = Number(prompt("salom son kiriting"));
+const son_2 = Number(prompt("salom son kiriting"));
+let natija = son_1 * son_2 
+let natijaa = son_1 / son_2
+let natijaaaa = son_1 - son_2
+let natijaaa = son_1 + son_2
+
+console.log(natija);
+console.log(natijaa);
+console.log(natijaaaa);
+console.log(natijaaa);
---
+++ New folder/app.js
@@ -0,0 +1,12 @@
+let x = 5;
+let y = 4;
+
+let formula = x / y
+let formulaa = x + y
+let formulaaa = x - y
+let formulaaaa = x * y
+
+console.log(formula);
+console.log(formulaa);
+console.log(formulaaa);
+console.log(formulaaaa);
